28a4fa9ced1a2c4944120536c5d7723cb7194e31
Dong Le Quoc Bao <quocbaohero2004@gmail.com>
2026-09-18 16:47:39 -0500

    This will show items in client section

--- account/client.py
+++ account/client.py
@@ -1,2 +1,8 @@
+from email_validator import validate_email, EmailNotValidError
 
-    
+class Client:
+    def __init__(self,
+                 client_id: str,
+                 name: str,
+                 email_address: str):
+        
