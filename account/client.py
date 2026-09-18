bf0627b98a2b2f41800d7981d3e9ea82b691e194
Dong Le Quoc Bao <quocbaohero2004@gmail.com>
2026-09-18 16:52:10 -0500

    Initialize client_id must be a positive number

--- account/client.py
+++ account/client.py
@@ -1,8 +1,18 @@
 from email_validator import validate_email, EmailNotValidError
 
-class Client:
+class Client:  #Prepresent items in the client section
     def __init__(self,
                  client_id: str,
                  name: str,
                  email_address: str):
-        
+
+        """Initialize the client object 
+          
+           Args:
+        Client_id (str): Unique identifier for the client.
+        Name (str): Name of the client.
+        Email_address (str): Email address of the client.
+        """
+
+        if client_id <= 0:
+            raise ValueError("Client ID must be a positive numeber.")
