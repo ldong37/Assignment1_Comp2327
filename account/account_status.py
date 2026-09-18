31cd3f73448bc045df70d9531eaa4b67149c73ed
Dong Le Quoc Bao <quocbaohero2004@gmail.com>
2026-09-18 16:31:46 -0500

    Add classified items for AccountStatus section

---
+++ account/account_status.py
@@ -0,0 +1,8 @@
+from enum import Enum   
+from decimal import Decimal
+
+class AccountStatus(Enum):
+    INACTIVE = 0
+    ACTIVE = 1
+    SUSPENDED = 2 
+    CLOSED = 3
--- account/client.py
+++ account/client.py
@@ -1,9 +1,2 @@
-from enum import Enum   
-from decimal import Decimal
 
-class AccountStatus(Enum):
-    INACTIVE = 0
-    ACTIVE = 1
-    SUSPENDED = 2 
-    CLOSED = 3
     
